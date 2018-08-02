0b2e26e12bb21f5c52f9884a578f07b27e15553e
bryan <bryan.hunt@erlang-solutions.com>
2018-08-02 11:23:20 +0100

    hello world endpoint working

--- apps/hello_world/src/hello_world_app.erl
+++ apps/hello_world/src/hello_world_app.erl
@@ -15,7 +15,37 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    hello_world_sup:start_link().
+    {ok, Pid} = hello_world_sup:start_link(),
+    Routes = [ {
+        '_',
+        [
+            {"/", hello_world_root, []}
+        ]
+    } ],
+
+
+
+
+
+
+    % Dispatch = cowboy_router:compile(Routes),
+
+    % NumAcceptors = 10,
+    TransOpts = [ {ip, {0,0,0,0}}, {port, 2938} ],
+    % ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+
+ ProtoOpts = #{                                                                                                                             
+        env => #{dispatch => cowboy_router:compile(Routes)}
+    % ,                                                                    
+        % max_frame_size_sent => MaxFrameSize                                                                                                    
+    },                                                                                                                                         
+    {ok, _} = cowboy:start_clear(foo, TransOpts, ProtoOpts),     
+
+
+    % {ok, _} = cowboy:start_clear(foobar,
+    %     NumAcceptors, TransOpts, ProtoOpts),
+
+    {ok, Pid}.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
